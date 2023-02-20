1444f373b147ebfc87ccbae7747530cc1c31a6bd
Mwaskev <mwasgit23@gmail.com>
2023-02-20 06:27:35 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -Entry point
+ * Description: prints allsingle digit numbers of
+ * base 10 from 0
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+	printf("%d\n", i);
+return (0);
+}
